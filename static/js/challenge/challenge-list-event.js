8ee2b9ba89cc841c7389d79ea234d00db9991134
jfk6725 <jfk6725@gmail.com>
2025-04-08 13:47:48 +0900

    화면 개발 완료

--- static/js/challenge/challenge-list-event.js
+++ static/js/challenge/challenge-list-event.js
@@ -1,7 +1,7 @@
-//  2025.04.07 조승찬
+//  2025.04.08 조승찬
 
 document.addEventListener("DOMContentLoaded", () => {
-    // 타이틀(스크랩, 좋아요, 댓글) 클릭 이벤트
+    // 타이틀(오늘, 주간, 월간 첼린지) 클릭 이벤트
     document.querySelectorAll(".MuiListItem-variantPlain").forEach((title) => {
         title.addEventListener("click", (e) => {
             e.preventDefault();
@@ -25,24 +25,5 @@ document.addEventListener("DOMContentLoaded", () => {
                 e.target.closest(".MuiListItem-variantPlain").classList.remove("joy-1t2bmje");
             }
         });
-    });
-
-    document.body.addEventListener("click", (e) => {
-        // 스크랩버튼 클릭 이벤트
-        if (e.target.closest(".joy-1ja7f1b")) {
-            if (e.target.closest(".joy-1ja7f1b").querySelector(".joy-17bmg2t")) {
-                // 스크랩 설정
-                e.target.closest(".joy-1ja7f1b").innerHTML = `
-                <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg" class="MuiSvgIcon-root MuiSvgIcon-sizeMd joy-1i55knr">
-                    <path d="M4.5 3.875v17.176a.95.95 0 0 0 1.496.777L12 17.625l6.004 4.203a.95.95 0 0 0 1.496-.777V3.875C19.5 2.84 18.66 2 17.625 2H6.375C5.34 2 4.5 2.84 4.5 3.875Z" fill="currentcolor"></path>
-                </svg>`;
-            } else {
-                // 스크랩 해제
-                e.target.closest(".joy-1ja7f1b").innerHTML = `
-                <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg" class="MuiSvgIcon-root MuiSvgIcon-sizeMd joy-17bmg2t">
-                    <path d="M4.5 3.875C4.5 2.84 5.34 2 6.375 2v17.242l5.082-3.629a.933.933 0 0 1 1.09 0l5.078 3.63V3.874H6.375V2h11.25c1.035 0 1.875.84 1.875 1.875v17.188a.938.938 0 0 1-1.48.762L12 17.526l-6.02 4.297a.938.938 0 0 1-1.48-.762V3.875Z" fill="currentcolor"></path>
-                </svg>`;
-            }
-        }
     });
 });
